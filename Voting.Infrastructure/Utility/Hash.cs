d9e5cc53d11e00956223522c71352476dc049ddd
Ahmad <Ahmad@DESKTOP-RUGDT66>
2019-10-21 00:21:49 +0330

    Unit Test initialized

---
+++ Voting.Infrastructure/Utility/Hash.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Votin.Model.Entities;
+
+namespace Voting.Infrastructure.Utility
+{
+    public static class Hash
+    {
+        public static byte[] HashBlock(this Block block)
+        {
+            string hashValue = $"{block.Timestamp}-{block.PreviousHash}-{block.Data}";
+
+            return HashString(hashValue);
+        }
+
+        public static byte[] HashString(string data)
+        {
+            return Encoding.UTF8.GetBytes(data);
+        }
+    }
+}
